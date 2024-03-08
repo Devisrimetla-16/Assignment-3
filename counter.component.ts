e7d4bbbc97dcf19eca5d9cc4f4ee2d2c0ccb8627
Devisrimetla-16 <144814143+Devisrimetla-16@users.noreply.github.com>
2024-03-08 16:43:28 +0530

    Add files via upload

---
+++ counter.component.ts
@@ -0,0 +1,36 @@
+// counter.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <div class="counter">
+      <h2>Counter: {{ counter }}</h2>
+      <button (click)="increment()">Increment</button>
+      <button (click)="decrement()">Decrement</button>
+    </div>
+  `,
+  styles: [`
+    .counter {
+      display: inline-block;
+      padding: 10px;
+      background-color: #87CEEB;
+      border-radius: 5px;
+    }
+    button {
+      margin: 5px;
+      background-color: #FFB6C1;
+    }
+  `]
+})
+export class CounterComponent {
+  counter = 0;
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+}
